605ca241ec265b62a18b00ec84c2410a609367dc
Friedrich Wicke <friedrich.wicke@protonmail.com>
2023-09-09 17:25:44 +0200

    syllabus page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -77,11 +77,13 @@ const config = {
             label: 'Tutorial',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
+          {to: '/syllabus', label: 'Syllabus', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
             position: 'right',
           },
+          
         ],
       },
       footer: {
